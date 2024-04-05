a30ed6881e34ef4b5b3b293a05af9d87edadbf60
DashaAndreeva <143691908+DashaAndreeva@users.noreply.github.com>
2024-04-05 17:43:04 +0300

    continue

--- src/components/imageCard/ImageCard.jsx
+++ src/components/imageCard/ImageCard.jsx
@@ -1,10 +1,8 @@
 export default function ImageCard({ image, openModal }) {
-  const { urls } = image;
-
   return (
     <div>
       <img
-        src={urls.small}
+        src={image.urls.small}
         alt=""
         onClick={() => {
           openModal(image);
--- src/components/imageGallery/ImageGallery.jsx
+++ src/components/imageGallery/ImageGallery.jsx
@@ -2,10 +2,12 @@ import ImageCard from "../imageCard/ImageCard";
 
 export default function ImageGallery({ images, openModal }) {
   return (
-    <ul>
-      {images.map((image) => (
-        <ImageCard key={image.id} image={image} openModal={openModal} />
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {images.map((image) => (
+          <ImageCard key={image.id} image={image} openModal={openModal} />
+        ))}
+      </ul>
+    </div>
   );
 }
